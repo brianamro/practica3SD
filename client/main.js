8b74eee3cf3ce5d96fc0997715c2638c733f67e3
RicentCB <ricardo.cb009@gmail.com>
2021-05-06 14:01:01 -0500

    [UP] Cambio en botones

--- client/main.js
+++ client/main.js
@@ -5,7 +5,7 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1200,
+    width: 900,
     height: 750,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
