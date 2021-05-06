29c67cc96b2b632b14abf6f9e6533383a49908f4
RicentCB <ricardo.cb009@gmail.com>
2021-05-06 16:47:07 -0500

    [UP] Ultimos cambios en la interfaz del cliente

--- client/main.js
+++ client/main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 750,
+    width: 800,
+    height: 650,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegrationInWorker: true,
