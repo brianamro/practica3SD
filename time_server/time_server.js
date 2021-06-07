8d00ab2f2a1b9f434c40b668884187ab182f799f
brianamro <brianamaro44@gmail.com>
2021-06-07 14:27:38 -0500

    TODO time server 2.0

--- time_server/time_server.js
+++ time_server/time_server.js
@@ -2,14 +2,9 @@ const net = require('net');
 const { setTimeout } = require('timers');
 
 
-const server = net.createServer((c) => {
-    // 'connection' listener.
-    console.log('client connected');
-    c.on('end', () => {
-      console.log('client disconnected');
-    });
-    c.write('hello\r\n');
-    c.pipe(c);
+const server = net.createServer();
+server.on('connection', (client) => {
+  console.log ('Client ')
 });
   
 server.on('error', (err) => {
@@ -19,6 +14,8 @@ server.on('error', (err) => {
 server.listen(8124, () => {
     console.log('time server bound');
 });
+
+
 
 
 
